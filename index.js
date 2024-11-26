815520c434ec7d83e0022089add7ae3de93abde4
Neto1919 <netomengao19@gmail.com>
2024-11-26 08:49:29 -0300

    Primeriro commit

---
+++ index.js
@@ -0,0 +1,79 @@
+let prompt = require("prompt-sync")()
+
+// Questão 01
+
+/*
+let idade = Number(prompt("Digite sua idade ?"));
+
+if (idade >= 18) {
+    console.log("Maior de idade.");
+} else {
+    console.log("Menor de idade.");
+}
+*/
+
+// Questão 02
+
+/*
+let numero = Number(prompt("Digite um número: "));
+
+if (numero % 2 === 0) {
+    console.log("Par");
+} else {
+    console.log("Impar");
+}
+*/
+
+// Questão 03
+
+/*
+let numero = Number(prompt("Digite um número: "));
+
+if(numero > 0) {
+    console.log("Positivo");
+} else if (numero < 0) {
+    console.log("Negativo");
+} else {
+    console.log("Zero");
+}
+*/
+
+// Questão 04
+
+/*
+let turno = prompt("Em que turno você estuda?");
+
+if(turno == "M"){
+    console.log("Bom dia");
+} else if (turno == "V"){
+    console.log("Boa tarde");
+} else if (turno == "N"){
+    console.log("Boa noite");
+} else {
+    console.log("Inválido");
+}
+*/
+// Questão 05
+
+/*
+let numero = Number(prompt("Digite um número: "))
+
+let resultado = (numero > 10) ? "Maior que 10" : "Menor que 10 ou igual a 10";
+console.log(resultado)
+*/
+
+// Questão 06
+
+/*
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+*/
+
+// Questão 07
+
+let numPadrao = 20;
+
+for(let i = 1; i <= numPadrao; i++){
+    console.log(i);
+}
